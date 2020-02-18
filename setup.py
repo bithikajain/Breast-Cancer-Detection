89febbeb25b67de420ae7d9568e756c1f8e738d1
Bithika Jain <jain.bithika01@gmail.com>
2020-02-18 17:22:52 -0500

    setup file included

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name = "InsightDatachallenge2", 
+     author= "Bithika Jain", 
+     author_email= "jain.bithika01@gmail.com", 
+     url = "https://github.com/bithikajain/InsightDataChallenge2", 
+     packages = ["PackageName"])
